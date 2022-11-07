16271be9615b0f2bc16a8eb17ef4d0c093cb3278
johngaalt <108419318+johngaalt@users.noreply.github.com>
2022-11-07 19:10:03 +0300

    Revert "init: done 1 and 2 task"
    This reverts commit a2275f9bce4a9f4b0612cb2d490c7d73005e341f.

--- 01-read-file/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-const stream = fs.createReadStream(path.join(__dirname, "text.txt"));
-
-stream.on("data", (n) => {
-  console.log(n.toString());
-});
--- 03-files-in-folder/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-fs.readdir(path.join(__dirname, "secret-folder"), "utf-8", (err, data) => {
-  if (err) throw err;
-  console.log(`В папке находятся файлы: ${data}`);
-});
