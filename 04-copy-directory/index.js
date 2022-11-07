376a13807160bd3de6c9d7745c58788f3a0234e4
johngaalt <108419318+johngaalt@users.noreply.github.com>
2022-11-07 20:38:07 +0300

    feat: copyFile

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require("fs");
+const { mkdir, copyFile } = require("node:fs/promises");
+const { join } = require("node:path");
 
+async function copyDirectory() {
+  const source = join(__dirname, "files");
+  const destination = join(__dirname, "files-copy");
+  const destinationPath = await mkdir(destination, { recursive: true });
+
+  if (!destinationPath) {
+    fs.readdir(source, { encoding: "utf-8" }, async (err, data) => {
+      if (err) throw err;
+
+      for (const file of data) {
+        console.log("PATH", destination + "\\" + file);
+        await copyFile(source, destination + "\\" + file);
+      }
+    });
+  }
+}
+
+copyDirectory().catch(console.error);
