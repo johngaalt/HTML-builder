2fb6f809cc30a062d85f7e702769e1b0cea6ac85
johngaalt <108419318+johngaalt@users.noreply.github.com>
2022-11-07 19:47:45 +0300

    fix: done task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,7 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.readdir(path.join(__dirname, "secret-folder"), "utf-8", (err, data) => {
-  if (err) throw err;
-  console.log(`В папке находятся файлы: ${data}`);
-});
+fs.readdir(
+  path.join(__dirname, "secret-folder"),
+  { encoding: "utf-8", withFileTypes: true },
+  (err, data) => {
+    if (err) throw err;
+
+    for (const item of data) {
+      if (!item.isDirectory()) {
+        const [name, extension] = item.name.split(".");
+        const pathToItem = path.join(__dirname, "secret-folder", item.name);
+        fs.stat(pathToItem, (err, { size }) => {
+          console.log(`${name} - ${extension} - ${size}b`);
+        });
+      }
+    }
+  }
+);
